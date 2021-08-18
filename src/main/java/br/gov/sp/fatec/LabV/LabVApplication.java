f158187e4d66dd0e00a3395ebaa91e49bf98bdf1
sandrotoline <sandro.toline@gmail.com>
2021-08-18 13:57:12 -0300

    initial commit

---
+++ src/main/java/br/gov/sp/fatec/LabV/LabVApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.LabV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabVApplication.class, args);
+	}
+
+}
